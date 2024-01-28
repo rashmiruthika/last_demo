384b185a4240b07c31a6516a9dcb26848605fd69
rashmiruthika <21bd1a05dccseg@gmail.com>
2024-01-28 19:07:51 +0530

    lastdemocommit

---
+++ src/main/java/EXAMPLE/lastdemo/App.java
@@ -0,0 +1,13 @@
+package EXAMPLE.lastdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Amigos!" );
+    }
+}
